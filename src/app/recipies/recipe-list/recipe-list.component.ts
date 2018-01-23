03fbb1eaa42025ed38ff9cb18971c2b7105110b8
BlagojeV93 <blagoje.vukovic@hotmail.com>
2018-01-23 09:39:56 +0100

    zapoceti recepies

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe-model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Princes krofne', 'Slatko u kurac', 'https://coolinarika.azureedge.net/images/_variations/0/1/01dc7f25b11472d661ecfa8670254af1_header.jpg')
+  ]
 
   constructor() { }
 
