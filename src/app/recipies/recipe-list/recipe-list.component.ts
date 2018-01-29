6b83a13f7c1d4b7ea0e460c432a6bbed9cc56c0a
BlagojeV93 <blagoje.vukovic@hotmail.com>
2018-01-29 16:45:12 +0100

    Pri kraju 06 sekcije

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe-model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe-model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeSelected2 = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Princes krofne', 'Slatko u kurac', 'http://www.recepti.com/img/recipe/31430-vocne-princes-krofne_zoom.jpg')
+    new Recipe('Princes krofne', 'Slatko u kurac', 'http://www.recepti.com/img/recipe/31430-vocne-princes-krofne_zoom.jpg'),
+    new Recipe('Papazjanija', 'OK jelo', 'http://secanja.com/wp-content/uploads/2015/09/papazjanija.jpg')
   ]
+  
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelected2(recipe: Recipe){
+    this.recipeSelected2.emit(recipe);
+  }
 }
